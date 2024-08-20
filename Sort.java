1ad3d90a4024c5b5f055a2db682094ee8565b05f
Tarantula-Jeff <113800958+Tarantula-Jeff@users.noreply.github.com>
2024-08-20 21:45:47 +0100

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- *
- * @author JEFFREY ACKAH AND Keil Barracliffe
+ * Author: Jeffrey Ackah and Keil Barracliffe
  */
 public class Sort {
 
@@ -23,24 +22,18 @@ public class Sort {
     }
 
     public void bubbleSort(ArrayList<Integer> numbers) {
-        // Boolean to hold whether the list is sorted or not
         boolean sorted = false;
 
-        //While loop to Keep looping until the list is sorted
         while (!sorted) {
-            // Set the sorted Boolean to true
-            // If we have to swap any items, this will be set to false
             sorted = true;
 
             for (int i = 0; i < numbers.size() - 1; i++) {
                 if (numbers.get(i) > numbers.get(i + 1)) {
-                    //swap
+                    // Swap elements
                     int temp = numbers.get(i);
                     numbers.set(i, numbers.get(i + 1));
                     numbers.set(i + 1, temp);
 
-                    // Since swap items, the list is not sorted
-                    // Set to sort
                     sorted = false;
                 }
             }
@@ -48,12 +41,9 @@ public class Sort {
     }
 
     public void shellSort(ArrayList<Integer> numbers) {
-        // Start with a gap of half the size of the list
         int gap = numbers.size() / 2;
 
-        // Continue until the gap is 1
         while (gap >= 1) {
-            // Perform an insertion sort on the list, using the current gap size
             for (int i = gap; i < numbers.size(); i++) {
                 int temp = numbers.get(i);
                 int j = i;
@@ -63,32 +53,23 @@ public class Sort {
                 }
                 numbers.set(j, temp);
             }
-
-            // Reduce the gap size by half
             gap /= 2;
         }
     }
 
-    public  void selectionSort(ArrayList<Integer> numbers) {
-        // Loop through the list of numbers
+    public void selectionSort(ArrayList<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
-            // Set the current item as the minimum
             int min = numbers.get(i);
             int minIndex = i;
 
-            // Loop through the remaining items in the list
             for (int j = i + 1; j < numbers.size(); j++) {
-                // Check if the current item is less than the minimum
                 if (numbers.get(j) < min) {
-                    // If it is, update the minimum and minimum index
                     min = numbers.get(j);
                     minIndex = j;
                 }
             }
 
-            // Check if the minimum is not the current item
             if (minIndex != i) {
-                // If it isn't, swap the current item with the minimum
                 int temp = numbers.get(i);
                 numbers.set(i, min);
                 numbers.set(minIndex, temp);
@@ -97,9 +78,7 @@ public class Sort {
     }
 
     public ArrayList<Integer> mergeSort(ArrayList<Integer> numbers) {
-        // Check if the list has more than one item
         if (numbers.size() > 1) {
-            // If it does, split the list into two halves
             ArrayList<Integer> left = new ArrayList<>();
             ArrayList<Integer> right = new ArrayList<>();
             int middle = numbers.size() / 2;
@@ -110,47 +89,34 @@ public class Sort {
                 right.add(numbers.get(i));
             }
 
-            // Recursively sort the two halves
             left = mergeSort(left);
             right = mergeSort(right);
 
-            // Merge the two sorted halves and return the result
             return merge(left, right);
         } else {
-            // If the list has only one item, return the list
             return numbers;
         }
     }
 
     public ArrayList<Integer> merge(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         ArrayList<Integer> finMerge = new ArrayList<>();
-        int m = list1.size();
-        int n = list2.size();
-        int i = 0, K = 0;
-        while (i < m && K < n) {
-            if (list1.get(i) < list2.get(K)) {
+        int i = 0, k = 0;
+        while (i < list1.size() && k < list2.size()) {
+            if (list1.get(i) < list2.get(k)) {
                 finMerge.add(list1.get(i));
                 i++;
-            } else if (list1.get(i) > list2.get(K)) {
-                finMerge.add(list2.get(K));
-                K++;
             } else {
-                finMerge.add(list1.get(i));
-                finMerge.add(list2.get(K));
-                i++;
-                K++;
+                finMerge.add(list2.get(k));
+                k++;
             }
         }
-        if (i == list1.size()) {
-            while (K < list2.size()) {
-                finMerge.add(list2.get(K));
-                K++;
-            }
-        } else {
-            while (i < list1.size()) {
-                finMerge.add(list1.get(i));
-                i++;
-            }
+        while (i < list1.size()) {
+            finMerge.add(list1.get(i));
+            i++;
+        }
+        while (k < list2.size()) {
+            finMerge.add(list2.get(k));
+            k++;
         }
         return finMerge;
     }
@@ -168,40 +134,32 @@ public class Sort {
     }
 
     public void heapSort(ArrayList<Integer> list) {
-        // Build the heap
         for (int i = list.size() / 2 - 1; i >= 0; i--) {
             heaping(list, list.size(), i);
         }
 
-        // Extract elements from the heap one by one
         for (int i = list.size() - 1; i >= 0; i--) {
-            // Move the current root (maximum value) to the end of the list
             int temp = list.get(0);
             list.set(0, list.get(i));
             list.set(i, temp);
 
-            // Call heaping on the reduced heap
             heaping(list, i, 0);
         }
     }
 
-    // Heapify the list
     public void heaping(ArrayList<Integer> list, int heapSize, int i) {
-        int largest = i; // Initialize the largest value as the root
-        int left = 2 * i + 1; // Index of the left child
-        int right = 2 * i + 2; // Index of the right child
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-        // If the left child is larger than the root
         if (left < heapSize && list.get(left) > list.get(largest)) {
             largest = left;
         }
 
-        // If the right child is larger than the root
         if (right < heapSize && list.get(right) > list.get(largest)) {
             largest = right;
         }
 
-        // If the largest value is not the root, swap it with the root and heaping the sublist
         if (largest != i) {
             int temp = list.get(i);
             list.set(i, list.get(largest));
